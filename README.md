b1c9500af9b2f246ed68aef2156bbedd889fc106
Jordan Jenkins <satdaten@gmail.com>
2013-06-02 16:04:49 -0500

    Updated Readme, changed setup script to install postgres correctly

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 A Vagrant Box - CentOSx64, Ruby, PostgreSQL
 ==============================
 
-This is another [Vagrant script](http://vagrantup.com), this time using a CentOS box to build a Ruby 2.0.0/PostgreSQL 8.4 set up for my introduction to programming class.
+This is another [Vagrant script](http://vagrantup.com), this time using a CentOS box to build a Ruby 1.9.3/PostgreSQL 8.4 set up for my introduction to programming class.
 
-This build script, rather than using Chef, uses a basic Bash script to install RVM, ruby 2.0.0, and all the dependencies for native gem installation, so a pretty good all around basic Ruby box. Very easy to modify to suite your needs.
+This build script, rather than using Chef, uses a basic Bash script to install RVM, ruby 1.9.3, and all the dependencies for native gem installation, so a pretty good all around basic Ruby box. Very easy to modify to suite your needs.
 
 One note for set up, you will need Virtualbox and Vagrant installed, then if teh base box doesn't automatically install, you can install it via this command:
 
--- setup.sh
+++ setup.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-mkdir /db
+mkdir /app
+chmod 0777 /app
 yum -y groupinstall "Development Tools"
-yum -y install expect openssl-devel libxml2 glibc gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel httpd nano postgresql-devel curl-devel httpd-devel apr-devel apr-util postgresql
+wget http://ftp.osuosl.org/pub/fedora-epel/6/i386/epel-release-6-8.noarch.rpm
+rpm -ivh epel-release-6-8.noarch.rpm
+yum -y install expect openssl-devel libxml2 glibc gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel httpd nano postgresql postgresql-server postgresql-contrib postgresql-devel curl-devel httpd-devel apr-devel apr-util
 curl -L https://get.rvm.io | bash -s stable --autolibs=3 --ruby
 source /usr/local/rvm/scripts/rvm
+rvm install 1.9.3
+rvm use 1.9.3
 gem install bundler
+
+adduser postgres
+
+echo -e "vagrant\nvagrant" | (passwd postgres --stdin)
+su postgres
+
+echo -e "exit\n"
 
 chmod -R 0777 /usr/local/rvm
 
-su vagrant
-
-source /usr/local/rvm/scripts/rvm
-echo "Installation Complete. You should have a working copy in /var/www/html of your local /code folder"
+echo "Installation Complete. You should have a working copy in /home/vagrant of your local /code folder. If you're going to be using PostgreSQL, run initdb -D /app/db -W -A md5 -U postgres and put in a password for the postgres user."
 exit
